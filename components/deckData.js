bd0494c56844cad3d2eab6c17f2150167bc0745b
Nathan Hartzell <nhartzel@uncc.edu>
2022-10-24 17:30:03 -0400

    updated deckdata to store more deck names

---
+++ components/deckData.js
@@ -0,0 +1,20 @@
+export const deckData = [
+    {
+        name: "East Deck",
+    },
+    {
+        name: "North Deck",
+    },
+    {
+        name: "CRI Deck",
+    },
+    {
+        name: "West Deck",
+    },
+    {
+        name: "Union Deck Lower"
+    },
+    {
+        name: "Union Deck Upper"
+    },
+];
