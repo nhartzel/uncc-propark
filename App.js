285fb818be16ba49ead3b9bfbd88ef373131b491
Nathan Hartzell <nhartzel@uncc.edu>
2022-10-24 15:26:19 -0400

    added deckData file for deck information storage

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Deck from './components/Deck'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <View style={styles.decksWrapper}>
+
+      
+
+        <Text style={styles.sectionTitle}>Decks</Text>
+        
+      
+        <View style={styles.entries}>
+          {/* This is where the decks will go */}
+          <Deck text={"North Deck"}/>
+          <Deck text={"West Deck"}/>
+        </View>
+
+      </View>
+
+
+
     </View>
   );
 }
@@ -14,8 +31,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8E8E8',
+    
+    
+  },
+  decksWrapper: {
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  entries: {
+    marginTop: 30,
   },
 });
