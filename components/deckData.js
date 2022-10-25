c2a738f9c50f43f6c5aa5537c47b5373d8f2f3bd
Nathan Hartzell <nhartzel@uncc.edu>
2022-10-24 23:55:25 -0400

    added passType field to deck data

--- components/deckData.js
+++ components/deckData.js
@@ -1,32 +1,42 @@
 export const deckData = [
     {
         name: "East Deck 1",
+        passType: "FV",
     },
     {
         name: "North Deck",
+        passType: "CRF",
     },
     {
         name: "CRI Deck",
+        passType: "CRF",
     },
     {
         name: "West Deck",
+        passType: "CF",
     },
     {
         name: "Union Deck Lower",
+        passType: "CF"
     },
     {
         name: "Union Deck Upper",
+        passType: "V",
     },
     {
-        name: "Cone Deck Visitor",
+        name: "Cone Deck 2",
+        passType: "CV",
     },
     {
-        name: "Cone Deck Faculty/Staff",
+        name: "Cone Deck 1",
+        passType: "F",
     },
     {
         name: "South Village Deck",
+        passType: "CRFV",
     },
     {
         name: "East Deck 2/3",
+        passType: "CF",
     },
 ];
