456a7a1d622a7cc32b2176a42f54a4f02a10c1cf
Nathan Hartzell <nhartzel@uncc.edu>
2022-10-28 09:02:06 -0400

    added boxes function and styles to schedule and settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -7,4 +7,51 @@ export default function SetteingsScreen({ navigation }) {
             <Text>Settings Screen</Text>
         </View>
     );
+
+    const Boxes = () => {
+        return(
+            <View style={styles.boxContainer}>
+    
+                <View style={styles.box}>
+                    <View style={styles.inner}>
+                        <Text>Profile</Text>
+                    </View>
+                </View>
+    
+                <View style={styles.box}>
+                    <View style={styles.inner}>
+                        <Text></Text>
+                    </View>
+                </View>
+    
+            </View>
+        )
+    }
+    
+    
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1
+    
+        },
+        boxContainer: {
+            width: '100%',
+            height: '100%',
+            padding: 5,
+            flexDirection: 'row',
+            flexWrap: 'wrap'
+        },
+        box: {
+            width: '90%',
+            height: '15%',
+            padding: 5
+        },
+        inner: {
+            flex: 1,
+            backgroundColor: 'c8c8c8',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }
+    
+    });
 };
