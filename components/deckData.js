82606bb0ef398e7a6f03c1083a197c9f66b96d2d
Nathan Hartzell <nhartzel@uncc.edu>
2022-10-27 20:35:26 -0400

    added deck coordinates to deckData.js

--- components/deckData.js
+++ components/deckData.js
@@ -2,41 +2,51 @@ export const deckData = [
     {
         name: "East Deck 1",
         passType: "FV",
+        coordinates: {latitude: 35.304997627808476, longitude:  -80.72749381592456},
     },
     {
         name: "North Deck",
         passType: "CRF",
+        coordinates: {latitude: 35.313681684376256, longitude: -80.73138124475983},
     },
     {
         name: "CRI Deck",
         passType: "CRF",
+        coordinates: {latitude: 35.3092170614558, longitude: -80.74338393708896},
     },
     {
         name: "West Deck",
         passType: "CF",
+        coordinates: {latitude: 35.305639992294715, longitude: -80.7364590582536},
     },
     {
         name: "Union Deck Lower",
-        passType: "CF"
+        passType: "CF",
+        coordinates: {latitude: 35.30930118436902, longitude: -80.73546267359546},
     },
     {
         name: "Union Deck Upper",
         passType: "V",
+        coordinates: {latitude: 35.30932744996379, longitude: -80.73549486010191},
     },
     {
         name: "Cone Deck 2",
         passType: "CV",
+        coordinates: {latitude: 35.30504958221708, longitude: -80.73433335825364},
     },
     {
         name: "Cone Deck 1",
         passType: "F",
+        coordinates: {latitude: 35.30455943736294, longitude: -80.73449754476007},
     },
     {
         name: "South Village Deck",
         passType: "CRFV",
+        coordinates: {latitude: 35.30101261454385, longitude: -80.73637635825376},
     },
     {
         name: "East Deck 2/3",
         passType: "CF",
+        coordinates: {latitude: 35.30551312311414, longitude: -80.72674186364696},
     },
 ];
